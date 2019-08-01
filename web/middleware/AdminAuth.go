49926ad45a96d0fea4d61bb8d735fea47bd9c661
xiaolin <462441355@qq.com>
2019-08-01 16:06:29 +0800

    * 增加jwt 登录校验

---
+++ web/middleware/AdminAuth.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-gw/config"
+	"go-gw/lib/jwt"
+	"net/http"
+)
+
+// 登录校验
+func AdminAuth(c *gin.Context)  {
+	tokenStr := c.Request.Header.Get("TOKEN")
+	if jwt.CheckValid(tokenStr, config.JWT_SECRET_ADMIN) == false {
+		c.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"success": false,
+			"ErrorCode": "700",
+			"msg": "登陆失败",
+		})
+		return
+	}
+
+	jwtData, err := jwt.ParseInfo(tokenStr, config.JWT_SECRET_ADMIN)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"success": false,
+			"ErrorCode": "700",
+			"msg": "登陆失败",
+		})
+		return
+	}
+	info, ok := jwtData["info"]
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"msg": "异常登录信息",
+		})
+		return
+	}
+	c.Set("admin", info.(map[string]interface{}))
+	c.Next()
+}
--- web/routes/admin.go
+++ web/routes/admin.go
@@ -3,30 +3,35 @@ package routes
 import (
 	"go-gw/bootstrap"
 	"go-gw/web/controller/admin"
+	"go-gw/web/middleware"
 )
 
 func AdminConfigure(b *bootstrap.Bootstrapper)  {
 	d := b.Group("/admin")
-	// 七牛
-	d.GET("/qiniu/get/token", new(admin.QiNiu).GetToken)
-	// 分类
-	d.POST("/category/add", new(admin.Category).CategoryAdd)
-	d.POST("/category/update", new(admin.Category).CategoryUpdate)
-	d.GET("/category/info", new(admin.Category).CategoryInfo)
-	d.POST("/category/update/status", new(admin.Category).CategoryUpdateStatus)
-	d.GET("/category/list", new(admin.Category).GetCategory)
-	d.GET("/category/get/all", new(admin.Category).GetCategoryAll)
-	// 文章
-	d.POST("/new/add", new(admin.New).NewAdd)
-	d.POST("/new/update", new(admin.New).NewUpdate)
-	d.POST("/new/update/status", new(admin.New).NewUpdateStatus)
-	d.GET("/new/info", new(admin.New).NewInfo)
-	d.GET("/new/list", new(admin.New).NewList)
-	//用户
-	d.POST("/user/add", new(admin.User).UserAdd)
-	d.POST("/user/update", new(admin.User).UserUpdate)
-	d.POST("/user/update/status", new(admin.User).UserUpdateStatus)
-	d.GET("/user/info", new(admin.User).UserInfo)
-	d.GET("/user/list", new(admin.User).UserList)
 	d.POST("/user/login", new(admin.User).Login)
+	
+	d.Use(middleware.AdminAuth)
+	{
+		// 七牛
+		d.GET("/qiniu/get/token", new(admin.QiNiu).GetToken)
+		// 分类
+		d.POST("/category/add", new(admin.Category).CategoryAdd)
+		d.POST("/category/update", new(admin.Category).CategoryUpdate)
+		d.GET("/category/info", new(admin.Category).CategoryInfo)
+		d.POST("/category/update/status", new(admin.Category).CategoryUpdateStatus)
+		d.GET("/category/list", new(admin.Category).GetCategory)
+		d.GET("/category/get/all", new(admin.Category).GetCategoryAll)
+		// 文章
+		d.POST("/new/add", new(admin.New).NewAdd)
+		d.POST("/new/update", new(admin.New).NewUpdate)
+		d.POST("/new/update/status", new(admin.New).NewUpdateStatus)
+		d.GET("/new/info", new(admin.New).NewInfo)
+		d.GET("/new/list", new(admin.New).NewList)
+		//用户
+		d.POST("/user/add", new(admin.User).UserAdd)
+		d.POST("/user/update", new(admin.User).UserUpdate)
+		d.POST("/user/update/status", new(admin.User).UserUpdateStatus)
+		d.GET("/user/info", new(admin.User).UserInfo)
+		d.GET("/user/list", new(admin.User).UserList)
+	}
 }
